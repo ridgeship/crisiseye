450dcaa48b5e8328ba325eef21459d64d96152e0
dela-99 <ridgedela444@gmail.com>
2026-07-15 19:49:02 +0000

    convex client provider fix

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -4,7 +4,7 @@ import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "https://dummy.convex.cloud");
 
 export default function ConvexClientProvider({
   children,
