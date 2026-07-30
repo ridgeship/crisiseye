126f40f2cce9559493157e391f0a93125472d8fd
dela-99 <ridgedela444@gmail.com>
2026-07-30 00:52:46 +0000

    fix: remove invalid suppressHydrationWarning prop from ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,6 @@ export default function RootLayout({
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-            suppressHydrationWarning
           >
             <ConvexClientProvider>
               <OfflineQueueProvider>
